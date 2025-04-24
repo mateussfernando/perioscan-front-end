3b137f6ea027391514042062eb8f7bb105657625
Bernardo Simões <151968699+b3rnardo15@users.noreply.github.com>
2025-04-24 03:35:45 +0000

    atualização nos icons

--- public/manifest.js
+++ public/manifest.js
@@ -1,6 +1,6 @@
 export default function manifest() {
     return {
-      name: "PerioScan - Perícias Odontolegais",
+      name: "PerioScan",
       short_name: "PerioScan",
       description: "Solução digital especializada no gerenciamento de perícias odontológicas forenses",
       start_url: "/",
@@ -10,50 +10,37 @@ export default function manifest() {
       icons: [
 
         {
-          src: "/images/icons/apple-touch-icon.png",
-          sizes: "46x46",
-          type: "image/png",
-        },
-        
-        {
-          src: "/images/icons/playstore.png",
-          sizes: "46x46",
-          type: "image/png",
-        },
-        {
           src: "/images/icons/web-app-manifest-192x192.png",
           sizes: "192x192",
           type: "image/png",
-          purpose: "any maskable",
+          purpose: "any maskable"
         },
         {
           src: "/images/icons/web-app-manifest-512x512.png",
           sizes: "512x512",
           type: "image/png",
-          purpose: "any maskable",
+          purpose: "any maskable"
         },
         {
           src: "/images/icons/logo-periocan114.png",
           sizes: "114x114",
-          type: "image/png",
+          type: "image/png"
         },
         {
           src: "/images/icons/logo-periocan196.png",
           sizes: "196x196",
-          type: "image/png",
+          type: "image/png"
         },
         {
           src: "/images/icons/logo-periocan256.png",
           sizes: "256x256",
-          type: "image/png",
+          type: "image/png"
         },
         {
           src: "/images/icons/logo-periocan512.png",
           sizes: "512x512",
-          type: "image/png",
-        },
-        
-
+          type: "image/png"
+        }
       ],
     }
   }
